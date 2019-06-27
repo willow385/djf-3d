2f8d90357a277c35f5bcc215a410d874d50360d5
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-06-26 22:43:30 -0500

    Add files via upload

---
+++ space_battle/main.cpp
@@ -0,0 +1,138 @@
+// Compile with the following:
+// g++ main.cpp `pkg-config --cflags --libs sdl2`
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#ifndef ENGINE
+#define ENGINE
+#include "../engine/djf-3d.hpp"
+#endif
+
+#include "sphere.hpp"
+
+#define GREEN 0, 255, 70
+#define BLACK 0, 0, 0
+
+int main() {
+    SDL_Renderer *renderer;
+    SDL_Window *main_window;
+    SDL_Init(SDL_INIT_VIDEO);
+    main_window = SDL_CreateWindow(
+        "Space Battle! (written in C++ by Dante Falzone)",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        0
+    );
+    renderer = SDL_CreateRenderer(
+        main_window,
+        0,
+        0
+    );
+    if (main_window == NULL || renderer == NULL) {
+        std::cout << "Error! Something went horribly wrong!\n";
+        std::cout << "Error: " << SDL_GetError() << '\n';
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(main_window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool window_open = true;
+    SDL_Event event;
+
+    Space space;
+    space.set_depth_to(800);
+    space.set_screen_center(300, 300);
+
+    Sphere universe;
+    universe.set_radius(150);
+    universe.set_space(space);
+    universe.set_centerpoint(
+        space.point_at(
+            300,
+            300,
+            100
+        )
+    );
+    universe.set_points();
+
+    while (window_open) {
+        SDL_SetRenderDrawColor(renderer, BLACK, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, GREEN, 255);
+        universe.draw_self(renderer);
+
+        render_string(
+            renderer,
+            1,
+            1,
+            "*** Controls ***",
+            16,
+            GREEN,
+            255
+        );
+        render_string(
+            renderer,
+            1,
+            11,
+            "WASD :: move up/down/left/right",
+            31,
+            GREEN,
+            255
+        );
+        render_string(
+            renderer,
+            1,
+            21,
+            "X/E  :: move forwards/backwards",
+            31,
+            GREEN,
+            255
+        );
+        SDL_RenderPresent(renderer);
+
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    window_open = false;
+                    break;
+                case SDL_KEYUP: break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_w:
+                            universe.y_trans(5);
+                            break;
+                        case SDLK_a:
+                            universe.x_trans(5);
+                            break;
+                        case SDLK_s:
+                            universe.y_trans(-5);
+                            break;
+                        case SDLK_d:
+                            universe.x_trans(-5);
+                            break;
+                        case SDLK_x:
+                            universe.z_trans(5);
+                            break;
+                        case SDLK_e:
+                            universe.z_trans(-5);
+                            break;
+                        case SDLK_UP:
+                            universe.x_rot_self(1);
+                            break;
+                    }
+                    break;
+                default: break;
+            }
+        }
+    }
+
+    /* if any heap memory was allocated, free it here */
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(main_window);
+    SDL_Quit();
+    return 0;
+}
---
+++ space_battle/sphere.hpp
@@ -0,0 +1,174 @@
+#ifndef ENGINE
+#define ENGINE
+#include "../engine/djf-3d.hpp"
+#endif
+
+#ifndef SDL2_SDL_H
+#define SDL2_SDL_H
+#include <SDL2/SDL.h>
+#endif
+
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif
+
+#ifndef RANDOM
+#define RANDOM
+#include <random>
+#endif
+
+#define POINT_CT 1024 // number of points in the sphere
+
+class Sphere {
+private:
+    float radius;
+    Space space;
+    CoordTriple centerpoint;
+    CoordTriple points[POINT_CT];
+
+    /* The following private method returns a number
+    that is greater than or equal to zero and less
+    than 360. */
+    float random_angle() {
+        std::random_device rd;
+        std::uniform_int_distribution<int> dist(0, 360);
+        return dist(rd);
+    }
+
+public:
+    /* methods for getting and setting */
+    float get_radius() {
+        return radius;
+    }
+
+    CoordTriple get_centerpoint() {
+        return centerpoint;
+    }
+
+    int set_radius(float value) {
+        radius = value;
+        return (radius == value ? 0 : 1);
+    }
+
+    int set_centerpoint(CoordTriple point) {
+        centerpoint.x = point.x;
+        centerpoint.y = point.y;
+        centerpoint.z = point.z;
+
+        if (centerpoint.x == point.x
+        &&  centerpoint.y == point.y
+        &&  centerpoint.z == point.z) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    int set_space(Space s) {
+        space = s;
+        return 0; // TODO: error handling
+    }
+
+    Space get_space() {
+        return space;
+    }
+
+    /* method for creating the points */
+    int set_points() {
+        for (int i = 0; i < POINT_CT; ++i) {
+            float theta = random_angle();
+
+            // Firstly: put the points at [radius] away
+            // from the centerpoint.
+            points[i] = space.point_at(
+                centerpoint.x,
+                centerpoint.y - radius,
+                centerpoint.z
+            );
+
+            // Then: perform rotations.
+            points[i] = space.z_rotation(
+                points[i],
+                centerpoint,
+                random_angle()
+            );
+            points[i] = space.y_rotation(
+                points[i],
+                centerpoint,
+                random_angle()
+            );
+        }
+        return 0;
+    }
+
+    /* method for drawing the damn thing already */
+    int draw_self(SDL_Renderer *renderer) {
+        int error_val;
+        for (int i = 0; i < POINT_CT; ++i) {
+            error_val += SDL_RenderDrawPoint(
+                renderer,
+                space.convert_to_2d(points[i]).x,
+                space.convert_to_2d(points[i]).y
+            );
+        }
+        return error_val;
+    }
+
+    /* method for transforming the sphere along the x-axis */
+    int x_trans(float distance) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i].x += distance;
+        }
+        return 0;
+    }
+
+    /* method for transforming the sphere along the y-axis */
+    int y_trans(float distance) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i].y += distance;
+        }
+        return 0;
+    }
+
+    /* method for transforming the sphere along the z-axis */
+    int z_trans(float distance) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i].z += distance;
+        }
+        return 0;
+    }
+
+
+    /* methods for rotating the sphere about itself */
+    int x_rot_self(float degrees) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i] = space.x_rotation(
+                points[i],
+                centerpoint,
+                degrees
+            );
+        }
+        return 0;
+    }
+    int y_rot_self(float degrees) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i] = space.y_rotation(
+                points[i],
+                centerpoint,
+                degrees
+            );
+        }
+        return 0;
+    }
+    int z_rot_self(float degrees) {
+        for (int i = 0; i < POINT_CT; ++i) {
+            points[i] = space.z_rotation(
+                points[i],
+                centerpoint,
+                degrees
+            );
+        }
+        return 0;
+    }
+};
